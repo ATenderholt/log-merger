ae7de6917bf6a4a060fd8b76bd9d7009923b6148
Adam Tenderholt <adam_tenderholt@targetdiscovery.com>
2021-03-17 20:22:07 -0700

    Update data generatoin script

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -5,8 +5,8 @@ import random
 
 start_time = datetime.datetime(2020, 10, 8, 0, 0, 0)
 with open('data.csv', 'w') as file:
-    for i, value in enumerate(itertools.product(string.ascii_lowercase, repeat=5)):
+    for i, value in enumerate(itertools.product(string.ascii_lowercase, repeat=6)):
         time = start_time + datetime.timedelta(seconds=i)
         event = ''.join(value)
-        sort = random.randrange(len(string.ascii_lowercase) ** 5)
+        sort = random.randrange(len(string.ascii_lowercase) ** 6)
         file.write('{}Z,{} - {} - {}\n'.format(time.isoformat(), event, i, sort))
